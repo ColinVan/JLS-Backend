a577776431abd26f7887da0ff54fd91d1ab076a3
QiuYue&YuXiaoyan <QiuYueYuXiaoyan@ByteDance.com>
2020-11-24 12:41:49 +0800

    jwt-login

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,7 @@
 const query = module.require("../handleDB/query.js")
+const { sign } = require("jsonwebtoken");
+const secret = "key";
+
 module.exports = async (ctx, next) => {
     console.log(ctx.request.body);/*{ username: 'laoba', password: '202cb962ac59075b964b07152d234b70' }*/
     let requestData =  ctx.request.body;
@@ -10,12 +13,11 @@ module.exports = async (ctx, next) => {
         + "\'";
     let data = await query(sql);
     if(data && data.length == 1){
+        console.log("login request legal, data is:");
         console.log(data);
-        ctx.body = data[0].password;
-    }else if(data){
-        console.log(data);
-        console.log("login middleWare is calling: No data.");
-        ctx.body = '';
+        const token = sign({user: requestData.username}, secret, { expiresIn: "1h"});
+        // ctx.body = data[0].password;
+        ctx.body = token;
     }else{
         console.log("login middleWare is calling:query not succeed.");
         ctx.body = '';
